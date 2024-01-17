309739984d7b7da171058c26d6949a6fa8dcb362
Shubham Thakur <shubham.thakur@lambdafunction.ai>
2024-01-17 17:41:57 +0530

    Using pylint

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,3 +1,4 @@
+"""noob file"""
 from django.db import models
 
 # Create your models here.
--- quickstart/views.py
+++ quickstart/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+"""noob file"""
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from quickstart.serializers import GroupSerializer, UserSerializers   
 
-from quickstart.serializers import GroupSerializer, UserSerializers
+class UserViewSet(viewsets.ModelViewSet): 
+    """Class represent"""
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.object.all().order_by("-date_joined")
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializers
     permission_classes = [permissions.IsAuthenticated]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
+    """Class represent"""
+
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
