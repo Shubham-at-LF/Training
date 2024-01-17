529bae3b2b3a109864b2104939892b0afb2d6394
Shubham Thakur <shubham.thakur@idsil.loc>
2024-01-17 11:00:09 +0530

    removed comment

--- Polls/models.py
+++ Polls/models.py
@@ -1,4 +1,3 @@
-
 import datetime
 from django.db import models
 from django.utils import timezone
@@ -7,7 +6,6 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-
     @admin.display(
         boolean=True,
         ordering="pub_date",
--- Polls/views.py
+++ Polls/views.py
@@ -18,9 +18,6 @@ class IndexView(generic.ListView):
     
 class DetailView(generic.DetailView):
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
     template_name  = "polls/detail.html"
 
